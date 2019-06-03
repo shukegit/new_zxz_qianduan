3084e8337248b5b1e3ba5fdae5ea89b1dee831cb
shukegit <2183217917@qq.com>
2019-06-04 01:04:37 +0800

    修改了点击next页面走过的距离

--- js/MY.js
+++ js/MY.js
@@ -1,6 +1,6 @@
 $(function() {
 	$(".main_next").click(function() {
 		/*跳转到第一个栏目所在的位置*/
-		$("html,body").animate({scrollTop:$(".section").offset().top - 80},1000);
+		$("html,body").animate({scrollTop:$(".section").offset().top - 60},1000);
  	});
 });
